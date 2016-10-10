7b9364b337b10d30cd3fb98973f8dc2f5be46790
Ali Nahid <anahid@salesforce.com>
2016-10-10 20:32:30 +1100

    sd

--- src/main/java/com/mcreceiverdemo/security/SecurityConfiguration.java
+++ src/main/java/com/mcreceiverdemo/security/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomAuthenticationProvider customAuthenticationProvider;
